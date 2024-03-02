6ed1c1bc7d2c70a127fdfea34816cf4ecc7035d5
Kylie Oliver <kylie.oliver@threatlocker.com>
2024-03-02 16:24:13 -0500

    created server

---
+++ server/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql2');
+
+const app = express()
+app.use(cors());
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "readyshoot"
+})
+
+app.get("/products", (req,res) => {
+    const sql =  "SELECT * FROM products";
+    db.query(sql, (err, data) => {
+        if(err) return res.json("Error");
+        return res.json(data);
+    })
+})
+
+app.listen(8081, () => {
+    console.log("Listening..")
+});
