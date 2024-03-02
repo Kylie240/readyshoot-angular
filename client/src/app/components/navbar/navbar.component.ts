33f0984871fde935500f1d5a288a196abc1a23dd
Kylie Oliver <olivkylie@gmail.com>
2024-03-02 11:24:42 -0500

    fixed routing issues

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
+import { ButtonModule } from 'primeng/button';
+
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
