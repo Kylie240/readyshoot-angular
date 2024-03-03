6d10b141f0f0e4a6a66ac0e0a9296090fa1add2f
Kylie Oliver <olivkylie@gmail.com>
2024-03-02 19:50:52 -0500

    updated server component

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql2');
+
+const app = express();
+app.use(cors());
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "readyshoot"
+})
+
+//products
+app.get("/products", (req,res) => {
+    const sql =  "SELECT * FROM products";
+    db.query(sql, (err, data) => {
+        if(err) return res.json("Error");
+        return res.json(data);
+    })
+})
+
+
+app.listen(8080, () => {
+    console.log("listening..")
+})
