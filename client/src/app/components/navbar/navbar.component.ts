8ab135d61b2a2f3e4bf5605fd35fc9e08f921b56
Kylie <olivkylie@gmail.com>
2024-03-03 13:02:52 -0500

    added mobile menu and navbar media query

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -2,14 +2,24 @@ import { Component } from '@angular/core';
 
 import { ButtonModule } from 'primeng/button';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [ButtonModule, RouterModule],
+  imports: [ButtonModule, RouterModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  isMobileMenuOpen: boolean = false;
 
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+    console.log(this.isMobileMenuOpen);
+  }
+
+  closeMobileMenu() {
+    this.isMobileMenuOpen = false;
+  }
 }
